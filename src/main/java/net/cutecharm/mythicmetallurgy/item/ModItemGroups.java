e54f35af76c41c384e884b6ef464ed3f917cfbf0
pleasestopyelling <cerachew@gmail.com>
2025-05-28 17:23:13 +0100

    some recipes

--- src/main/java/net/cutecharm/mythicmetallurgy/item/ModItemGroups.java
+++ src/main/java/net/cutecharm/mythicmetallurgy/item/ModItemGroups.java
@@ -16,6 +16,18 @@ public class ModItemGroups {
                     .icon(() -> new ItemStack(ModItems.CRUSHED_RAW_ADAMANTITE)).entries((displayContext, entries) -> {
                         entries.add(ModItems.CRUSHED_RAW_ADAMANTITE);
                         entries.add(ModItems.CRUSHED_RAW_AQUARIUM);
+                        entries.add(ModItems.CRUSHED_RAW_BANGLUM);
+                        entries.add(ModItems.CRUSHED_RAW_CARMOT);
+                        entries.add(ModItems.CRUSHED_RAW_KYBER);
+                        entries.add(ModItems.CRUSHED_RAW_MANGANESE);
+                        entries.add(ModItems.CRUSHED_RAW_MIDAS_GOLD);
+                        entries.add(ModItems.CRUSHED_RAW_MYTHRIL);
+                        entries.add(ModItems.CRUSHED_RAW_ORICHALCUM);
+                        entries.add(ModItems.CRUSHED_RAW_PALLADIUM);
+                        entries.add(ModItems.CRUSHED_RAW_PROMETHEUM);
+                        entries.add(ModItems.CRUSHED_RAW_QUADRILLUM);
+                        entries.add(ModItems.CRUSHED_RAW_RUNITE);
+                        entries.add(ModItems.CRUSHED_RAW_STORMYX);
 
                     }).build());
 
--- src/main/java/net/cutecharm/mythicmetallurgy/item/ModItems.java
+++ src/main/java/net/cutecharm/mythicmetallurgy/item/ModItems.java
@@ -10,6 +10,18 @@ import net.minecraft.util.Identifier;
 public class ModItems {
     public static final Item CRUSHED_RAW_ADAMANTITE = registerItem("crushed_raw_adamantite", new Item(new FabricItemSettings()));
     public static final Item CRUSHED_RAW_AQUARIUM = registerItem("crushed_raw_aquarium", new Item(new FabricItemSettings()));
+    public static final Item CRUSHED_RAW_BANGLUM = registerItem("crushed_raw_banglum", new Item(new FabricItemSettings()));
+    public static final Item CRUSHED_RAW_CARMOT = registerItem("crushed_raw_carmot", new Item(new FabricItemSettings()));
+    public static final Item CRUSHED_RAW_KYBER = registerItem("crushed_raw_kyber", new Item(new FabricItemSettings()));
+    public static final Item CRUSHED_RAW_MANGANESE = registerItem("crushed_raw_manganese", new Item(new FabricItemSettings()));
+    public static final Item CRUSHED_RAW_MIDAS_GOLD = registerItem("crushed_raw_midas_gold", new Item(new FabricItemSettings()));
+    public static final Item CRUSHED_RAW_MYTHRIL = registerItem("crushed_raw_mythril", new Item(new FabricItemSettings()));
+    public static final Item CRUSHED_RAW_ORICHALCUM = registerItem("crushed_raw_orichalcum", new Item(new FabricItemSettings()));
+    public static final Item CRUSHED_RAW_PALLADIUM = registerItem("crushed_raw_palladium", new Item(new FabricItemSettings()));
+    public static final Item CRUSHED_RAW_PROMETHEUM = registerItem("crushed_raw_prometheum", new Item(new FabricItemSettings()));
+    public static final Item CRUSHED_RAW_QUADRILLUM = registerItem("crushed_raw_quadrillum", new Item(new FabricItemSettings()));
+    public static final Item CRUSHED_RAW_RUNITE = registerItem("crushed_raw_runite", new Item(new FabricItemSettings()));
+    public static final Item CRUSHED_RAW_STORMYX = registerItem("crushed_raw_stormyx", new Item(new FabricItemSettings()));
 
     private static Item registerItem(String name, Item item) {
         return Registry.register(Registries.ITEM, new Identifier(CreateMythicMetallurgy.MOD_ID, name), item);
