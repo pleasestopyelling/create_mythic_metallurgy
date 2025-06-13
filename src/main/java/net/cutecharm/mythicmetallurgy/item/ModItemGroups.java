b7625c8c67dedf5a1bed665bcc05e54e27aabc5b
pleasestopyelling <cerachew@gmail.com>
2025-06-13 22:19:13 +0100

    no idea tbh

--- src/main/java/net/cutecharm/mythicmetallurgy/item/ModItemGroups.java
+++ src/main/java/net/cutecharm/mythicmetallurgy/item/ModItemGroups.java
@@ -32,6 +32,6 @@ public class ModItemGroups {
                     }).build());
 
     public static void registerItemGroups() {
-        CreateMythicMetallurgy.LOGGER.info("Registering item groups for" + CreateMythicMetallurgy.MOD_ID);
+        CreateMythicMetallurgy.LOGGER.info("Registering item groups for " + CreateMythicMetallurgy.MOD_ID);
     }
 }
